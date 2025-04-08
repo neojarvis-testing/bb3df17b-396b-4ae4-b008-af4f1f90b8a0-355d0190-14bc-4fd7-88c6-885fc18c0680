3af5187dd6f6894bd27f381f982fe8772797379b
Suneela42 <suneelacheekati42@gmail.com>
2025-04-08 06:40:25 +0000

    Admin Nav Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReviewComponent } from './review/review.component';
+import { AdminnavComponent } from './components/adminnav/adminnav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReviewComponent,
+    AdminnavComponent
   ],
   imports: [
     BrowserModule,
