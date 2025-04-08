ac5de8c0ea9dd1c3768d7798b3ebaacbeb604c6b
Harshal010703 <harshalrajput867@gmail.com>
2025-04-08 13:23:07 +0000

    08-04-2025 6:50pm springapp added oder oredrrepo orderservice orderserviceimpl

--- springapp/src/main/java/com/examly/springapp/model/Order.java
+++ springapp/src/main/java/com/examly/springapp/model/Order.java
@@ -1,5 +1,123 @@
 package com.examly.springapp.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Order {
 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+    private LocalDate orderDate;
+    private String orderStatus;
+    private String shippingAddress;
+    private String billingAddress;
+    private Double totalAmount;
+
+    @ManyToOne
+    @JoinColumn(name="userId")
+    private User user;
+
+    @OneToMany(mappedBy="order" , cascade=CascadeType.ALL)
+    private List<OrderItem> orderItems;
+
+    public Order() {
+    }
+
+    public Order(Long orderId, LocalDate orderDate, String orderStatus, String shippingAddress, String billingAddress,
+            Double totalAmount, User user, List<OrderItem> orderItems) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.shippingAddress = shippingAddress;
+        this.billingAddress = billingAddress;
+        this.totalAmount = totalAmount;
+        this.user = user;
+        this.orderItems = orderItems;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(String shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public String getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(String billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    @Override
+    public String toString() {
+        return "Order [orderId=" + orderId + ", orderDate=" + orderDate + ", orderStatus=" + orderStatus
+                + ", shippingAddress=" + shippingAddress + ", billingAddress=" + billingAddress + ", totalAmount="
+                + totalAmount + ", user=" + user + ", orderItems=" + orderItems + "]";
+    }
+
+
+    
+
+
+    
 }
