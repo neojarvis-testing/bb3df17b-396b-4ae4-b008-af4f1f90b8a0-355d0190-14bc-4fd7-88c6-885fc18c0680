bdc636672f4cd036c7eb9ed7acde48cbc6390259
1Rushikesh-Deshmukh <rushideshmukh0118@gmail.com>
2025-04-10 04:13:24 +0000

    10-04-2025 9:44 modify review component service

--- angularapp/src/app/services/review.service.ts
+++ angularapp/src/app/services/review.service.ts
@@ -1,11 +1,34 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Review } from '../models/review.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReviewService {
+  private baseUrl:string="https://ide-cdeacfbeedaedadafbbadbcfdcfcc.premiumproject.examly.io/proxy/8080/api/reviews";
 
-  private baseUrl:string="";
+  constructor(private httpClient:HttpClient) { }
+  addReview(review:Review):Observable<any>{
+    return this.httpClient.post(this.baseUrl,review);
+  }
+  getReviewById(reviewId:number):Observable<any>{
+    return this.httpClient.get(this.baseUrl+"/"+reviewId);
+  }
+  getAllReviews():Observable<any>{
+    return this.httpClient.get(this.baseUrl);
+  }
+  getReviewsByUserId(userId:number):Observable<any>{
+    return this.httpClient.get(this.baseUrl+"/user/"+userId);
+  }
+  getReviewsByProductId(productId:number):Observable<any>{
+    return this.httpClient.get(this.baseUrl+"/product/"+productId);
+  }
 
-  constructor() { }
+  deleteReview(reviewId:number):Observable<any>{
+  return this.httpClient.delete(this.baseUrl+"/"+reviewId);
+  }
+
 }
+
