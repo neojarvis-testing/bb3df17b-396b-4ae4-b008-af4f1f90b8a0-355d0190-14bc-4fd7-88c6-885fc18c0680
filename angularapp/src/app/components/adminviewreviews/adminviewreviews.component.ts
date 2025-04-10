59dfba90a1d8a0926f90c7c51fa5e8fcce731bda
1Rushikesh-Deshmukh <rushideshmukh0118@gmail.com>
2025-04-10 10:19:21 +0000

    10-04-2025 3:50 modify adminviewreviews

--- angularapp/src/app/components/adminviewreviews/adminviewreviews.component.ts
+++ angularapp/src/app/components/adminviewreviews/adminviewreviews.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from 'src/app/models/product.model';
+import { Review } from 'src/app/models/review.model';
+import { ProductService } from 'src/app/services/product.service';
+import { ReviewService } from 'src/app/services/review.service';
 
 @Component({
   selector: 'app-adminviewreviews',
@@ -6,10 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adminviewreviews.component.css']
 })
 export class AdminviewreviewsComponent implements OnInit {
-
-  constructor() { }
+  reviews:Review [] =[];
+  product :Product = {
+    productName: "", price: 0, category: "", brand: "", description: "",
+    stockQuantity: 0,
+    coverImage: ''
+  };
+  productId:number;
+  constructor(private reviewService :ReviewService,private router :Router,private activateRoute:ActivatedRoute,private productService:ProductService) { }
 
   ngOnInit(): void {
+    this.getAllReviews();
+    this.productId =  parseInt(this.activateRoute.snapshot.paramMap.get('id'));
+    this.productService.getProductById(this.productId).subscribe(data=>{
+      this.product = data;
+    })
+  }
+  getAllReviews(){
+    this.reviewService.getAllReviews().subscribe(data=>{
+      this.reviews = data;
+    })
   }
 
 }
+
--- angularapp/src/app/components/review/review.component.ts
+++ angularapp/src/app/components/review/review.component.ts
@@ -1,8 +1,5 @@
-import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { ProductLauncher } from 'puppeteer';
-import { Review } from 'src/app/models/review.model';
-import { ReviewService } from 'src/app/services/review.service';
+
 
 @Component({
   selector: 'app-review',
@@ -10,16 +7,12 @@ import { ReviewService } from 'src/app/services/review.service';
   styleUrls: ['./review.component.css']
 })
 export class ReviewComponent implements OnInit {
-  reviews:Review [] =[];
-  constructor(private reviewService :ReviewService) { }
+ 
+  constructor() { }
 
   ngOnInit(): void {
-    this.getAllReviews();
+    
   }
-  getAllReviews(){
-    this.reviewService.getAllReviews().subscribe(data=>{
-      this.reviews = data;
-    })
-  }
+
 
 }
