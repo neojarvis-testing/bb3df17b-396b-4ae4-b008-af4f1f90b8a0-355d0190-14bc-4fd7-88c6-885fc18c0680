ff675f11fdada09201812450322539089cd3a53a
Suneela42 <suneelacheekati42@gmail.com>
2025-04-17 12:56:17 +0000

    Extra changes

--- angularapp/src/app/components/checkout/checkout.component.ts
+++ angularapp/src/app/components/checkout/checkout.component.ts
@@ -117,4 +117,9 @@ export class CheckoutComponent implements OnInit {
       }
     });
   }
+
+  closePopup() {
+    this.isPopupVisible = false; // Hide the popup
+    this.router.navigate(['/user-view-product']); // Redirect to View Products page
+}
 }
